cc0051df4b4f351b5381136554616e79382ad785
Melodypapa <melodypapa@outlook.com>
2021-05-11 04:31:10 +0000

    add the container support

--- src/spreadsheet/main.cpp
+++ src/spreadsheet/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include "Cell.h"
 
 void printCell(const SpreadSheetCell & cell){
@@ -12,4 +13,11 @@ int main(){
     anotherCell.setString("3.2");
     std::cout << "Cell 1: " << myCell.getValue() << std::endl;
     std::cout << "Cell 2: " << anotherCell.getValue() << std::endl;
+
+    SpreadSheetCell * myCellp = new SpreadSheetCell();
+    myCellp->setValue(3.7);
+    std::cout << "cell 1: " << myCellp->getValue() << " " << myCellp->getString() << std::endl;
+    delete myCellp;
+    myCellp = nullptr;
+
 }
