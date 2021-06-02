b8133615cb7ea7a8cbc565768c50fff31faced66
melodypapa <melodypapa@outlook.com>
2021-06-02 20:08:10 +0800

    add the wide char example

--- src/strings/string_class.cpp
+++ src/strings/string_class.cpp
@@ -2,89 +2,99 @@
 #include <string>
 #include <charconv>
 #include <array>
+#include <cstdio>
+#include <clocale>
 
 void simple_string()
 {
-    std::string A("12");
-    std::string B("34");
-    std::string C;
+   std::string A("12");
+   std::string B("34");
+   std::string C;
 
-    C = A + B;
-    std::cout << C << std::endl;
+   C = A + B;
+   std::cout << C << std::endl;
+}
+
+void wide_string()
+{
+   const wchar_t *myString = L"Tschüs";
+
+   setlocale(LC_ALL, "");
+
+   printf("wide character: <%ls> \n", myString);
 }
 
 void string_compare()
 {
-    std::string myString = "hello";
-    myString += ", there";
-    std::string myOtherString = myString;
-    if (myString == myOtherString)
-    {
-        myOtherString[0] = 'H';
-    }
-    std::cout << myString << std::endl;
-    std::cout << myOtherString.c_str() << std::endl;
+   std::string myString = "hello";
+   myString += ", there";
+   std::string myOtherString = myString;
+   if (myString == myOtherString) {
+      myOtherString[0] = 'H';
+   }
+   std::cout << myString << std::endl;
+   std::cout << myOtherString.c_str() << std::endl;
 }
 
 void string_literals()
 {
-    using namespace std::string_literals;
+   using namespace std::string_literals;
 
-    std::string s1 = "abc\0\0def";
-    std::string s2 = "abc\0\0def"s;
-    std::cout << "s1: " << s1.size() << " \"" << s1 << "\"\n";
-    std::cout << "s2: " << s2.size() << " \"" << s2 << "\"\n";
+   std::string s1 = "abc\0\0def";
+   std::string s2 = "abc\0\0def"s;
+   std::cout << "s1: " << s1.size() << " \"" << s1 << "\"\n";
+   std::cout << "s2: " << s2.size() << " \"" << s2 << "\"\n";
 }
 
 void convert_to_string()
 {
-    long double d = 3.14L;
-    std::string s = std::to_string(d);
-    std::cout << s << std::endl;
+   long double d = 3.14L;
+   std::string s = std::to_string(d);
+   std::cout << s << std::endl;
 }
 
 void parse_string()
 {
-    const std::string toParse = "    123USD";
-    size_t index = 0;
-    int value = stoi(toParse, &index);
-    std::cout << "Parsed value: " << value << std::endl;
-    std::cout << "First non-parsed character: '" << toParse[index] << "'" << std::endl;
+   const std::string toParse = "    123USD";
+   size_t            index   = 0;
+   int               value   = stoi(toParse, &index);
+   std::cout << "Parsed value: " << value << std::endl;
+   std::cout << "First non-parsed character: '" << toParse[index] << "'" << std::endl;
 
-    value = stoi(toParse, nullptr, 16);
-    std::cout << "Parsed value: " << value << std::endl;
+   value = stoi(toParse, nullptr, 16);
+   std::cout << "Parsed value: " << value << std::endl;
 }
 
 void to_chars()
 {
-    std::string out(10, ' ');
-    auto result = std::to_chars(out.data(), out.data() + out.size(), 12345);
-    if (result.ec == std::errc())
-    {
-        std::cout << out.size() << " \"" << out << "\"" << std::endl;
-    }
+   std::string out(10, ' ');
+   auto        result = std::to_chars(out.data(), out.data() + out.size(), 12345);
+   if (result.ec == std::errc()) {
+      std::cout << out.size() << " \"" << out << "\"" << std::endl;
+   }
 
-    std::array<char, 10> str;
+   std::array<char, 10> str;
 
-    if (auto [ptr, ec] = std::to_chars(str.data(), str.data() + str.size(), 42);
-        ec == std::errc())
-    {
-        std::cout << str.size() << " \"" << std::string_view(str.data(), ptr - str.data()) << "\"" << std::endl;
-        // C++17, uses string_view(ptr, length
-    }
+   if (auto [ptr, ec] = std::to_chars(str.data(), str.data() + str.size(), 42);
+       ec == std::errc()) {
+      std::cout << str.size() << " \"" << std::string_view(str.data(), ptr - str.data()) << "\"" << std::endl;
+      // C++17, uses string_view(ptr, length
+   }
 }
 
 int main()
 {
-    simple_string();
+   simple_string();
 
-    string_compare();
+   wide_string();
 
-    string_literals();
+   string_compare();
 
-    convert_to_string();
+   string_literals();
 
-    parse_string();
+   convert_to_string();
 
-    to_chars();
+   parse_string();
+
+   to_chars();
 }
